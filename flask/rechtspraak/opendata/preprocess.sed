8ced61390fa443bc903b9bbef29db34efebe63a9
joost soeterbroek <joost.soeterbroek@gmail.com>
2018-08-26 20:19:17 +0200

    add preprocess rules with sed parser

---
+++ flask/rechtspraak/opendata/preprocess.sed
@@ -0,0 +1,7 @@
+/tail/d
+s/@{http:\/\/www.w3.org\/2000\/01\/rdf-schema#}//
+s/@{http:\/\/www.w3.org\/1999\/02\/22-rdf-syntax-ns#}//
+s/#text/text/
+s/@scheme/scheme/
+s/@resourceIdentifier/resourceidentifier/
+s/"resourceidentifier": "..\/..\/rs:inhoudsindicatie",/"resourceidentifier": "..\/..\/rs:inhoudsindicatie"/
--- flask/rechtspraak/opendata/preprocess.sh
+++ flask/rechtspraak/opendata/preprocess.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 ONETIMEZIPFILE="OpenDataUitspraken.zip"
 # eenmalig, iedere maand update
@@ -12,30 +13,40 @@ else
 fi
 
 # assume unpacked
-for folder in uitspraken/*/; do
-  cd "${folder%/}"
+CURRENT_PWD_BASENAME=$(basename $(pwd))
+CURRENT_PWD=$(pwd)
+#for folder in uitspraken/*/; do
+for folder in uitspraken/2010/; do
+  #echo "------- $folder"
+  cd "$folder"
       if [ -f .unzipped ]; then
           echo "nothing to unzip in ${folder%/}"
       else
           echo "unzipping in ${folder%/}"
-          for file in *.zip; do
+          shopt -s nullglob
+          for f in *.zip; do
+              fbname=$(basename "$f" .zip)
               echo "unzipping ${file}"
-              unzip -f $file > /dev/null
-
-              for xmlfile in *.xml; do
-                  fbname=$(basename "$xmlfile" .xml)
-                  jsname="$fbname.json"
-                  xmlname="$fbname.xml"
-                  #echo "DEBUG $fbname $jsname $xmlname"
-                  if [ ! -f $jsname ]; then
-                      echo "xml2json $xmlfile"
-                      python ../../xml2json.py --pretty --strip_namespace --strip_newlines -t xml2json -o $jsname $xmlname
-                  fi
-                  touch $jsname
-              done
-
+              unzip -f $f > /dev/null
           done
           touch .unzipped
+
       fi
-  cd -
+      for xmlfile in *.xml; do
+          fbname=$(basename "$xmlfile" .xml)
+          jsname="$fbname.json"
+          xmlname="$fbname.xml"
+          #echo "DEBUG $fbname $jsname $xmlname"
+          if [ ! -f $jsname ]; then
+              echo "xml2json $fbname"
+              python ../../xml2json.py --pretty --strip_namespace --strip_newlines -t xml2json -o $jsname $xmlname
+              /usr/bin/sed -i -f ../../preprocess.sed $jsname || exit 1
+              cat $jsname | json_verify -u || mv $jsname $jsname.invalid
+              echo "compressing $xmlname"
+              gzip $xmlname
+          else
+              echo "skip $fbname"
+          fi
+      done
+  cd $CURRENT_PWD
 done
