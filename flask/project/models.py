bd7b345809a5d0aba7a8c1352a7d18bdf4059432
joost soeterbroek <joost.soeterbroek@gmail.com>
2018-08-27 18:12:36 +0200

    add to uitspraak model

--- flask/project/models.py
+++ flask/project/models.py
@@ -40,16 +40,31 @@ class Uitspraak(db.Model):
 
     __tablename__ = "uitspraak"
 
-    id            = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    registered_on = db.Column(db.DateTime, nullable=False)
-    identifier    = db.Column(db.String(25), default = '', unique=True)
+    id              = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    registered_on   = db.Column(db.DateTime, nullable=False)
+    identifier      = db.Column(db.String(25), default = '', unique=True)
+    modified        = db.Column(db.String(255), nullable=False)
+    publicatiedatum = db.Column(db.String(255), nullable=False)
+    uitspraakdatum  = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, identifier = ''):
-        self.registered_on = datetime.datetime.now()
-        self.identifier = identifier
+    def __init__(self, identifier = '', modified, publicatiedatum, uitspraakdatum):
+        self.registered_on   = datetime.datetime.now()
+        self.identifier      = identifier
+        self.modified        = modified
+        self.publicatiedatum = publicatiedatum
+        self.uitspraakdatum  = uitspraakdatum
 
     def get_id(self):
         return self.id
+
+    def get_modified(self):
+        return self.modified
+
+    def get_publicatiedatum(self):
+        return self.publicatiedatum
+
+    def get_uitspraakdatum(self):
+        return self.uitspraakdatum
 
     def __repr__(self):
         return '<Identifier {0}>'.format(self.identifier)
