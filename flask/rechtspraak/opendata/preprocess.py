8450b0cba4073cabf199bbcbe675b74b40587840
joost soeterbroek <joost.soeterbroek@gmail.com>
2018-08-28 03:13:32 +0200

    first rudimentary preprocessor with lxml

--- flask/rechtspraak/opendata/preprocess.py
+++ flask/rechtspraak/opendata/preprocess.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+from lxml import etree
 from flask import Flask, render_template, url_for, json
 from gurdjieff import app, db
 from gurdjieff.models import Uitspraak
@@ -9,25 +10,38 @@ def process_zip_to_xml(zip):
 
 def process_xml_to_db(year):
 
-    _dir = 'rechtspraak/opendata/uitspraken'
+    _dir = os.path.join('rechtspraak/opendata/uitspraken', year) 
     print(_dir)
-    #d = open(os.path.join(_dir, year, json_str), "r")
 
-    #data = json.load(d)
+    # namespaces map
+    # xmlns:dcterms="http://purl.org/dc/terms/"
+    # xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    nsmap = {
+            'dcterms': 'http://purl.org/dc/terms/',
+            'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+            }
 
-    #print(data['open-rechtspraak']['RDF']['Description'])
+    for _file in os.listdir(_dir):
+        if 'xml' in _file:
+            _f = os.path.join(_dir, _file) 
+            print("INFO: parsing %s" % _f)
+            with open(_f, 'r', encoding="utf-8") as content:
+                tree = etree.parse(content)
+                root = tree.getroot()
 
-    #data_root = data['open-rechtspraak']['RDF']['Description']
-    #identifier = data_root['identifier'].get('text')
-    ##print("DEBUG: Identifier: %s" % identifier)
-    #modified = data_root['modified'].get('text')
-    #publicatiedatum = data_root['issued'].get('text')
-    #uitspraakdatum = data_root['date'].get('text')
-    #zaaknummer = data_root['zaaknummer'].get('text')
+                lst_identifier = root.findall('.//dcterms:identifier', namespaces=nsmap)
+                for l in lst_identifier:
+                    identifier = l.text
+                #print("DEBUG: Identifier: %s" % identifier)
 
-    ## add and commit to db
-    #u = Uitspraak(identifier, modified, publicatiedatum, uitspraakdatum, zaaknummer)
-    #exists = db.session.query(Uitspraak.id).filter_by(identifier=identifier).scalar() is not None
-    #if not exists:
-    #    db.session.add(u)
-    #    db.session.commit()
+                modified = 'foo' #FIXME
+                publicatiedatum = 'foo' #FIXME
+                uitspraakdatum = 'foo' #FIXME
+                zaaknummer = 'foo' #FIXME
+
+                # add and commit to db
+                u = Uitspraak(identifier, modified, publicatiedatum, uitspraakdatum, zaaknummer)
+                exists = db.session.query(Uitspraak.id).filter_by(identifier=identifier).scalar() is not None
+                if not exists:
+                    db.session.add(u)
+                    db.session.commit()
